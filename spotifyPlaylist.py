33ba09213ba29f436b44d30f9f838798f2c978df
Muhammad Daniyal Saqib <muhammaddaniyalsaqib@Muhammads-MacBook-Pro.local>
2019-08-10 15:28:32 -0400

    submodules

--- spotifyPlaylist.py
+++ spotifyPlaylist.py
@@ -1,19 +1,26 @@
 import os
 import sys
+import spotipy
+from spotipy import util
 import json
-spotipy = sys.path.insert(0, './spotipy/util.py')
 import webbrowser
 from json.decoder import JSONDecodeError
 
 #Get the username from terminal
-username = sys.argv[1]
+username = '222lflk6h5pichsj4goxz64zi'
+client_id = 'd92a8649deee4f2cb07d8f023647b829'
+client_secret = '6d3e00471d1c4930b49f17b5847e17ec'
+redirect_uri = 'http://google.com/'
 
 #User ID: 222lflk6h5pichsj4goxz64zi
+
+
+scope = 'user-library-read'
 try:
-	token = util.prompt_for_user_token(username)
-except:
-	os.remove(f".cache-{username}")
-	token = util.prompt_for_user_token(username)
+    token = util.prompt_for_user_token(username, None, client_id, client_secret, redirect_uri)
+except (AttributeError, JSONDecodeError):
+    os.remove(f".cache-{username}")
+    token = util.prompt_for_user_token(username, None, client_id, client_secret, redirect_uri)
 
 #Create Spotipy Object
 spotifyObject = spotipy.Spotify(auth=token)
